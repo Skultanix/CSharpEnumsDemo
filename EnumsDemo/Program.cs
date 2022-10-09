307f30562127f45459aef27b4488a349c0b8b50d
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-10-08 23:15:42 -0600

    Initial Enum setup complete. Initial examples of pulling data from enums complete.

--- EnumsDemo/Program.cs
+++ EnumsDemo/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace EnumsDemo
 {
+    public enum ShippingMethod
+    {
+        PolarExpress = 1,
+        CorgiCaravan = 2,
+        CarrierPigeon = 3
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var method = ShippingMethod.PolarExpress;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+
         }
     }
 }
